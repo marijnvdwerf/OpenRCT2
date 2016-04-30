76e66dd25b4e20972a80b2be930901287fe0c2a9
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2016-04-30 22:09:53 +0200

    Start decompilation of 0x6A3590

--- src/world/map.h
+++ src/world/map.h
@@ -27,7 +27,7 @@ typedef struct rct_map_element_surface_properties {
 } rct_map_element_surface_properties;
 
 typedef struct rct_map_element_path_properties {
-	uint8 type; //4 0xF0 Path type, 0x08 Unknown/Unused, 0x04 Set when path is diagonal, 0x03 Rotation
+	uint8 type; //4 0xF0 Path type, 0x08 Ride sign, 0x04 Set when path is diagonal, 0x03 Rotation
 	uint8 additions; //5
 	uint8 edges; //6
 	union {
