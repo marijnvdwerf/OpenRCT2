13dddc2803394b1acf34c7d31c6f8a69cf8504eb
duncanspumpkin <duncans_pumpkin@hotmail.co.uk>
2016-06-06 17:21:54 +0100

    Finish function. Readd path additions. Fix scrolling text

--- src/world/scenery.h
+++ src/world/scenery.h
@@ -128,7 +128,7 @@ typedef enum {
 
 typedef struct rct_path_bit_scenery_entry {
 	uint16 var_06;
-	uint8 pad_08;
+	uint8 var_08;
 	uint8 tool_id;			// 0x09
 	sint16 price;			// 0x0A
 	uint8 scenery_tab_id;	// 0x0C
